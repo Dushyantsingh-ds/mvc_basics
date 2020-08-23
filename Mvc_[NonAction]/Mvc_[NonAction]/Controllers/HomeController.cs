03603b52734eb2728537f14e76ededd82f6ca403
Dushyant <dushyantsingh.dsindia@gmail.com>
2020-08-23 07:29:54 -0700

    Intial Commit

---
+++ Mvc_[NonAction]/Mvc_[NonAction]/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_Basics_One.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Show_Controller()
+        {
+            return "This is a Controller Without any View";
+        }// it just like Controller
+
+        [NonAction]
+        public string Show_Function()
+        {
+            return "This is a Controller Without any View";
+        }// it just like Controller
+
+    }
+}
---
+++ Mvc_[NonAction]/Mvc_[NonAction]/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mvc_Basics_One.MvcApplication" Language="C#" %>
